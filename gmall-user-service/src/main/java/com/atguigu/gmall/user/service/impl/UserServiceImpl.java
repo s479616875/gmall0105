16841b4f59a3619e8963fc792eb118a56fc6ae54
sunshaoyang <479616875@qq.com>
2020-12-23 22:21:47 +0800

    谷粒多商城dubbo框架整合

---
+++ gmall-user-service/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.atguigu.gmall.user.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.bean.UmsMember;
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.service.UserService;
+import com.atguigu.gmall.user.mapper.UmsMemberReceiveAddressMapper;
+import com.atguigu.gmall.user.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import java.util.List;
+//引入dubbo服务service注解
+//接口实现类
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    UmsMemberReceiveAddressMapper umsMemberReceiveAddressMapper;
+    @Override
+    public List<UmsMember> getAllUser() {
+        //通用mapper执行单表的增删改查操作
+        List<UmsMember> useMemberList = userMapper.selectAll(); //userMapper.selectAllUser();
+        return useMemberList;
+    }
+
+    @Override
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMember(String memberId) {
+        UmsMemberReceiveAddress umsMemberReceiveAddress = new UmsMemberReceiveAddress();
+        umsMemberReceiveAddress.setMemberId(memberId);
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = umsMemberReceiveAddressMapper.select(umsMemberReceiveAddress);
+
+        return umsMemberReceiveAddresses;
+    }
+}
--- gmall-user-web/src/main/java/com/atguigu/gmall/user/GmallUserWebApplication.java
+++ gmall-user-web/src/main/java/com/atguigu/gmall/user/GmallUserWebApplication.java
@@ -2,14 +2,12 @@ package com.atguigu.gmall.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.atguigu.gmall.user.mapper")
-public class GmallUserServiceApplication {
+public class GmallUserWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GmallUserServiceApplication.class, args);
+        SpringApplication.run(GmallUserWebApplication.class, args);
     }
 
 }
